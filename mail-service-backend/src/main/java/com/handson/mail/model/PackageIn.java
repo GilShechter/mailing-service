2aa6faa9d7aced5d7ad89d101e688d82e5778e18
Gil Shechter <gilshechter13@gmail.com>
2024-03-11 23:13:26 +0200

    Getters and setters for PackageIn

--- mail-service-backend/src/main/java/com/handson/mail/model/PackageIn.java
+++ mail-service-backend/src/main/java/com/handson/mail/model/PackageIn.java
@@ -24,6 +24,38 @@ public class PackageIn implements Serializable {
     @Enumerated(EnumType.STRING)
     private PackageType type;
 
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public void setDestinationAddress(String destinationAddress) {
+        this.destinationAddress = destinationAddress;
+    }
+
+    public String getDestinationZipCode() {
+        return destinationZipCode;
+    }
+
+    public void setDestinationZipCode(String destinationZipCode) {
+        this.destinationZipCode = destinationZipCode;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public PackageType getType() {
+        return type;
+    }
+
+    public void setType(PackageType type) {
+        this.type = type;
+    }
+
     public Package toPackage() {
         return aPackage().createdAt(Dates.nowUTC()).destinationAddress(destinationAddress)
                 .destinationZipCode(destinationZipCode).recipientName(recipientName).type(type)
